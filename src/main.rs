54507e98ecccde539e6d4744418a7914e1812ed0
Jcampos <jcamposforner@gmail.com>
2019-08-15 19:09:39 +0200

    Distancias

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,66 @@
 use geo::prelude::*;
 use geo::Point;
 use std::cmp::Ordering::Equal;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Player {
+    point: Point::<f64>,
+    name: String,
+}
+
+impl Player {
+    fn new(point: Point::<f64>, name: &'static str) -> Self {
+        Player {
+            point,
+            name: name.to_string(),
+        }
+    }
+}
 
 fn main() {
     let p = |x, y| Point::<f64>::from((x, y));
 
-    let place = p(-74.006, 40.7128);
+    let places = vec![
+        p(-74.006, 40.7128),
+        p(-68.1278, 53.5074),
+        p(-61.1278, 58.5074),
+    ];
     let players = vec![
-        p(-0.1278, 51.5074),
-        p(-73.1278, 40.5074),
-        p(-65.1278, 51.5074),
+        Player::new(p(-0.1278, 51.5074), "Jesus"),
+        Player::new(p(61.1278, 58.5074), "a"),
+        Player::new(p(-61.1278, 58.5074), "d"),
     ];
 
-    let mut distances = Vec::new();
+    let mut distances: HashMap<String, Vec<f64>> = HashMap::new();
 
     for player in players.into_iter() {
-        distances.push(player.haversine_distance(&place));
+        let mut vec_of_points: Vec<f64> = Vec::new();
+        let places = places.clone();
+        
+        for place in places.into_iter() {
+            match distances.get(&player.name) {
+                Some(_) => {
+                    vec_of_points.push(player.point.haversine_distance(&place));
+                    distances.insert(
+                        player.name.clone(),
+                        vec_of_points.clone(),
+                    );
+                }
+                None => {
+                    vec_of_points.push(player.point.haversine_distance(&place));
+                    distances.insert(
+                        player.name.clone(),
+                        vec_of_points.clone(),
+                    );
+                }
+            }
+        }
     }
 
-    println!("{}", distances.iter().cloned().fold(0. / 0., f64::min));
-    distances.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
-    println!("{:?}", distances);
+    for (player, mut distance) in distances {
+        println!("{} closed distance {}", player, distance.iter().cloned().fold(0. / 0., f64::min));
+        distance.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
+        println!("{:?}", distance);
+    }
 }
